fcd8d736be44bf9fd3fc33318a345c2f2466a0d7
omar-sheta <omar-sheta@aucegypt.edu>
2022-11-22 17:47:40 +0200

    server1

---
+++ server1/hello/target/debug/build/libc-eb88f1e18b0e0091/build_script_build-eb88f1e18b0e0091.d
@@ -0,0 +1,5 @@
+/Users/Omar/Desktop/client_agent_server/server/hello/target/debug/build/libc-eb88f1e18b0e0091/build_script_build-eb88f1e18b0e0091: /Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.137/build.rs
+
+/Users/Omar/Desktop/client_agent_server/server/hello/target/debug/build/libc-eb88f1e18b0e0091/build_script_build-eb88f1e18b0e0091.d: /Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.137/build.rs
+
+/Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.137/build.rs:
---
+++ server1/hello/target/debug/deps/thiserror_impl-50b719ec77b95b4f.d
@@ -0,0 +1,12 @@
+/Users/Omar/Desktop/client_agent_server/server/hello/target/debug/deps/libthiserror_impl-50b719ec77b95b4f.dylib: /Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/thiserror-impl-1.0.37/src/lib.rs /Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/thiserror-impl-1.0.37/src/ast.rs /Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/thiserror-impl-1.0.37/src/attr.rs /Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/thiserror-impl-1.0.37/src/expand.rs /Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/thiserror-impl-1.0.37/src/fmt.rs /Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/thiserror-impl-1.0.37/src/generics.rs /Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/thiserror-impl-1.0.37/src/prop.rs /Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/thiserror-impl-1.0.37/src/valid.rs
+
+/Users/Omar/Desktop/client_agent_server/server/hello/target/debug/deps/thiserror_impl-50b719ec77b95b4f.d: /Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/thiserror-impl-1.0.37/src/lib.rs /Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/thiserror-impl-1.0.37/src/ast.rs /Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/thiserror-impl-1.0.37/src/attr.rs /Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/thiserror-impl-1.0.37/src/expand.rs /Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/thiserror-impl-1.0.37/src/fmt.rs /Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/thiserror-impl-1.0.37/src/generics.rs /Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/thiserror-impl-1.0.37/src/prop.rs /Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/thiserror-impl-1.0.37/src/valid.rs
+
+/Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/thiserror-impl-1.0.37/src/lib.rs:
+/Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/thiserror-impl-1.0.37/src/ast.rs:
+/Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/thiserror-impl-1.0.37/src/attr.rs:
+/Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/thiserror-impl-1.0.37/src/expand.rs:
+/Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/thiserror-impl-1.0.37/src/fmt.rs:
+/Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/thiserror-impl-1.0.37/src/generics.rs:
+/Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/thiserror-impl-1.0.37/src/prop.rs:
+/Users/Omar/.cargo/registry/src/github.com-1ecc6299db9ec823/thiserror-impl-1.0.37/src/valid.rs:
